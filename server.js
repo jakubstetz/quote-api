dc44ec85f9aeeeed2c33aca6d849110c6b653e59
Hugo <119013233+hugo-ste@users.noreply.github.com>
2024-09-28 16:25:11 -0400

    Open GET /api/quotes/random route (Task 4)

--- server.js
+++ server.js
@@ -9,3 +9,7 @@ const PORT = process.env.PORT || 4001;
 app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
+app.get('/api/quotes/random', (req, res, next) => {
+  res.send({quote: getRandomElement(quotes)});
+})
